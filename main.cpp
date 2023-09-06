9b9b0c97beb24c344906cb1e8a197399ea378304
Tiago Costa <tafcosta89@gmail.com>
2023-09-06 23:47:27 +0200

    defined dijkstra class

--- main.cpp
+++ main.cpp
@@ -1,24 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
+#define INF std::numeric_limits<double>::infinity()
+
 class Graph{
 public:
-  Graph(int nVertex) : adj_list(nVertex) {}
+  Graph(int nVertex) : adj_list(nVertex), dist_toOtherVertices(nVertex, vector<double>(nVertex, INF)), nVertex(nVertex) {}
 
   void add_edge(int u, int v, double distance){
     adj_list[u].emplace_back(v, distance);
     adj_list[v].emplace_back(u, distance);
   }
 
+  vector<vector<double>> dist_toOtherVertices;
+
   int get_nVertex(){
     return nVertex;
   }
   
 private:
-  vector<vector<pair<int, int>>> adj_list;
+  vector<vector<pair<int, double>>> adj_list;
   int nVertex;
+};
+
+class Dijkstra{
+public:
+  Dijkstra(Graph& graph, int SrcVertex) : graph_(graph), SrcVertex(SrcVertex){
+    graph_.dist_toOtherVertices[SrcVertex][SrcVertex] = 0.0;
+    nVertex = graph_.get_nVertex();
+  }
+
+private:
+  Graph& graph_;
+  int nVertex;
+  int SrcVertex;
 };
 
 int main(){
